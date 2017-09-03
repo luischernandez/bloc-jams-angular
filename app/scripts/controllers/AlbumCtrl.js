91c3bc58c15f7ee9821ef7a8fd4a8a097bf71ee1
luischernandez <luishernandez01@live.com>
2017-09-03 15:12:30 -0700

    Create a SongPlayer service

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,12 +1,13 @@
 (function() {
-    function AlbumCtrl(Fixtures) {
+    function AlbumCtrl(Fixtures, SongPlayer) {
         this.heroTitle = "Turn the music up!";
         this.albumData = Fixtures.getAlbum();
+        this.songPlayer = SongPlayer;
         //add albumData property that holds copy of albumPicasso
         //use ng-repeat
     }
 
     angular
         .module('blocJams')
-        .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
+        .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
 })();
