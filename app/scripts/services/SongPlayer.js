efd457b074adec8f053a8c3f7b17d00ff997efff
luischernandez <luishernandez01@live.com>
2017-09-03 16:08:42 -0700

    Create playSong function and documentation

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -3,17 +3,17 @@
         var SongPlayer = {};
 
         var currentSong = null;
-/*
-@desc Buzz object audio file
-@type {Object}
-*/
+        /*
+        * @desc Buzz object audio file
+        * @type {Object}
+        */
         var currentBuzzObject = null;
 
-/*
-@function setSong
-@desc Stops currently playing song and loads new audio file as currentBuzzObject
-@param {Object} song
-*/
+        /*
+        * @function setSong
+        * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+        * @param {Object} song
+        */
         var setSong = function(song){
             if(currentBuzzObject){
                 currentBuzzObject.stop();
@@ -26,19 +26,34 @@
             currentSong = song;
         };
 
+        //Assignment cp7 pt2
+        /*
+        * @function: playSong
+        * @desc: plays the current song and sets the song.playing property to true
+        * @param: {Object} song
+        */
+        var playSong = function(song){
+            currentBuzzObject.play();
+            song.playing = true;
+        };
+
+        /*
+        * @desc: sets current song and plays song. calls playSong function
+        */
         SongPlayer.play = function(song){
             if(currentSong !== song){
-
                 setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong(song);
             } else if(currentSong === song){
                 if (currentBuzzObject.isPaused()) {
-                    currentBuzzObject.play();
+                    playSong(song);
                 }
             }
         };
 
+        /*
+        * @desc: pauses current song, sets song.playing to false
+        */
         SongPlayer.pause = function(song) {
             currentBuzzObject.pause();
             song.playing = false;
